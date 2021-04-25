d37ed81d61addb6f1e47d57573b61100a08a0dab
Ruben Karlsson <neburgordon@gmail.com>
2021-04-25 21:53:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv/config");
+require("./db");
+const express = require("express");
+const app = express();
+require("./config")(app);
+
+// TITLE
+app.locals.title = `For now: iceCreamProject`;
+
+// ROUTES
+const index = require("./routes/index");
+app.use("/", index);
+
+// ERROR HANDLiNG
+require("./error-handling")(app);
+
+module.exports = app;
---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("icecream-project JS imported successfully!");
+  },
+  false
+);
