b89fbac4fd099f7e00896270b2ebb89e20963542
Ruben Karlsson <neburgordon@gmail.com>
2021-04-27 17:28:23 +0200

    fix passport sending messages to views & cloudinary transform image to fit faces

--- routes/auth.js
+++ routes/auth.js
@@ -28,15 +28,24 @@ router.get('/login', notLoggedInCheck(), (req, res) => {
   res.render('auth/login');
 });
 
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    passReqToCallback: true,
-    failureFlash: true,
-  }),
-);
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.render('auth/login', info);
+    }
+
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      return res.render('profile', { user });
+    });
+  })(req, res, next);
+});
 
 router.get('/logout', (req, res) => {
   req.logout();
